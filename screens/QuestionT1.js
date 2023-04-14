88e57bc944575b79b914e20a726594b5c7e1da9a
ralucaantal <antalvaidaraluca20@stud.ase.ro>
2023-04-14 20:26:05 +0300

    S-a facut pagina pentru primul tip de intrebare.

--- screens/QuestionT1.js
+++ screens/QuestionT1.js
@@ -1,15 +1,33 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { ArrowLeftIcon, LightBulbIcon } from "react-native-heroicons/solid";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { themeColors } from "../theme/index";
+import { RadioButton } from "react-native-paper";
 
 const detaliiCont = ["Zile ⚡", "Puncte 🚀", "Vieți 🤍"];
 
+const intrebarePropusa = [
+  {
+    id: 1,
+    textIntrebare:
+      "Textul pentru întrebarea cu numărul 1 este acesta. Care este răspunsul corect?",
+    varianta1: "Varianta 1 de răspuns",
+    varianta2: "Varianta 2 de răspuns",
+    varianta3: "Varianta 3 de răspuns",
+  },
+];
+
 export default function QuestionT1() {
   const navigation = useNavigation();
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const handlePress = (value) => {
+    setSelectedValue(value);
+  };
+
   return (
     <LinearGradient
       colors={["rgba(135, 125, 250, 0.9)", "rgba(180, 174, 232, 0.7)"]}
@@ -71,13 +89,13 @@ export default function QuestionT1() {
             flexDirection: "row",
           }}
         >
-          <LightBulbIcon color={themeColors.galben} size="50" />
+          <LightBulbIcon color={themeColors.galben} size="50" style={{opacity: 0.6}}/>
           <View className="flex-1 flex justify-center pl-3 space-y-3">
             <Text
-              style={{ color: themeColors.white,fontSize: 20}}
+              style={{ color: themeColors.white, fontSize: 20 }}
               className="font-semibold"
             >
-              Textul pentru întrebarea cu numărul 1 este acesta. Care este răspunsul corect?
+              {intrebarePropusa[0].textIntrebare}
             </Text>
           </View>
         </View>
@@ -85,7 +103,84 @@ export default function QuestionT1() {
           style={{ height: "100%", marginTop: 20 }}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
-        ></ScrollView>
+        >
+          <View
+            className="mx-4 p-2 mb-2 flex-row"
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0.2)",
+              borderRadius: 10,
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <ScrollView className="form space-y-2" style={{ width: "100%" }}>
+              <Text className="text-white ml-4">Alege răspunsul corect: </Text>
+
+              <View style={{ marginTop: 10 }}>
+                <TouchableOpacity
+                  onPress={() => handlePress("varianta1")}
+                  style={{ flexDirection: "row", alignItems: "center" }}
+                >
+                  <RadioButton
+                    value="varianta1"
+                    status={
+                      selectedValue === "varianta1" ? "checked" : "unchecked"
+                    }
+                    onPress={() => handlePress("varianta1")}
+                    color={themeColors.galben}
+                  />
+                  <Text style={{ color: "white", marginLeft: 10 }}>
+                    {intrebarePropusa[0].varianta1}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => handlePress("varianta2")}
+                  style={{ flexDirection: "row", alignItems: "center" }}
+                >
+                  <RadioButton
+                    value="varianta2"
+                    status={
+                      selectedValue === "varianta2" ? "checked" : "unchecked"
+                    }
+                    onPress={() => handlePress("varianta2")}
+                    color={themeColors.galben}
+                  />
+                  <Text style={{ color: "white", marginLeft: 10 }}>
+                    {intrebarePropusa[0].varianta2}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => handlePress("varianta3")}
+                  style={{ flexDirection: "row", alignItems: "center" }}
+                >
+                  <RadioButton
+                    value="varianta3"
+                    status={
+                      selectedValue === "varianta3" ? "checked" : "unchecked"
+                    }
+                    onPress={() => handlePress("varianta3")}
+                    color={themeColors.galben}
+                  />
+                  <Text style={{ color: "white", marginLeft: 10 }}>
+                    {intrebarePropusa[0].varianta3}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity
+                className="py-3 bg-yellow-400 rounded-xl"
+                style={{ width: "30%", opacity: 0.8, alignSelf: "flex-end" }}
+                onPress={() => {
+                  // console.log("S-au facut modificari pt utilizator");
+                  // navigation.navigate("Profile");
+                }}
+              >
+                <Text className="font-xl font-bold text-center text-gray-700">
+                  Next
+                </Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </LinearGradient>
   );
