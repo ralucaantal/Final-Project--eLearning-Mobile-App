38fbdf2e5a9c170c27feebbc19c7763142982dc1
ralucaantal <antalvaidaraluca20@stud.ase.ro>
2023-06-15 22:31:45 +0300

    m am apucat de documentatie

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const acasa = "http://192.168.1.128";
 const hotspot = "http://172.20.10.3";
 const alex = "http://172.20.10.2";
 const eu = "http://172.20.10.3";
-const bacau = "http://192.168.1.20";
+const bacau = "http://192.168.1.5";
 const bacauZ = "http://192.168.0.165";
 const facultate="http://10.30.25.205"
 
 
 let IPv4;
-export default IPv4 = acasa;
+export default IPv4 = bacau;
